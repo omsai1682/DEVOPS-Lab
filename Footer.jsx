82927cc2e4d18aab41847cecca31323d57c03d3d
omsai1682 <2303a51682@sru.edu.in>
2026-09-17 16:27:11 +0530

    Delete Footer.jsx

--- Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="dashboard-footer">
-      <p>&copy; 2026 Student Management System. All rights reserved.</p>
-    </footer>
-  );
-}
